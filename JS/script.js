1e9493577d33d4679311048d88b3457cb4b3caa9
Dastan <kyylshbek66@gmail.com>
2021-12-10 14:05:59 -0800

    attempting con.log

--- JS/script.js
+++ JS/script.js
@@ -4,9 +4,18 @@ let darkButton = document.getElementById('dark-theme')
 let body = document.getElementById('body');
 let buttons = document.getElementById('toggle').addEventListener('click', myFunction)
 
-function myFunction()
+function myFunction(){
     buttons.onclick = () => {
         body.classList.replace(lightButton, darkButton)
-
+    }
 }
 
+let allinputs = document.getElementById('email')
+let allinputs2 = document.getElementById('name')
+
+document.querySelectorAll('submit').addEventListener('click', function (e){
+    e.preventDefault();
+
+    console.log(allinputs.value)
+    console.log(allinputs2.value)
+})
